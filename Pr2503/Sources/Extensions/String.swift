7ceb8f3e0e1c80dcfcf6b8fa623be4d051e48a26
Eugenius Morozov <emorozov777@gmail.com>
2022-07-21 17:45:54 +0300

    Make random generated password.

--- Pr2503/Sources/Extensions/String.swift
+++ Pr2503/Sources/Extensions/String.swift
@@ -6,12 +6,13 @@
 //
 
 extension String {
-    var digits:      String { return "0123456789" }
-    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
-    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
-    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
-    var letters:     String { return lowercase + uppercase }
-    var printable:   String { return digits + letters + punctuation}
+    var digits:           String { return "0123456789" }
+    var lowercase:        String { return "abcdefghijklmnopqrstuvwxyz" }
+    var uppercase:        String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
+    var punctuation:      String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
+    var letters:          String { return lowercase + uppercase }
+    var lettersAndDigits: String { return letters + digits }
+    var printable:        String { return letters + digits + punctuation}
 
     mutating func replace(at index: Int, with character: Character) {
         var stringArray = Array(self)
